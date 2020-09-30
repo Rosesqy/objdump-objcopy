8809f07b245ee32caba7295d0e1ec53bdd96779b
Qingyang Shi <Rosesqy@users.noreply.github.com>
2020-10-01 02:19:56 +0800

    tb

--- getsections.c
+++ getsections.c
@@ -3,39 +3,7 @@
 #include <fcntl.h>
 #include "objsect.h"
 
-
-//static char *default_target = "elf64-x86-64";
-//char *sep = "  ";
 static char *nextln = "\n";
-
-// static void print_objdump(bfd *abfd, asection *sect, void *obj){
-	
-// 	const char *namep;
-
-// 	namep = bfd_section_name(abfd,sect);
-// 	write(1,namep,strlen(namep));	
-// 	write(1,sep,strlen(sep));
-
-// 	bfd_vma addr = bfd_section_vma(abfd,sect);
-// 	// printf("%lx  ",addr);
-// 	write(1,&addr,sizeof(addr));
-// 	write(1,sep,strlen(sep));
-
-// 	unsigned long size = bfd_section_size(abfd, sect);
-// 	write(1,&size, sizeof(size));
-// 	write(1,sep,strlen(sep));
-
-// 	long store = bfd_get_symtab_upper_bound(abfd);
-// 	// printf("%lx  ",size);
-// 	// write(1,&store,sizeof(store));
-// 	// write(1,sep,strlen(sep));
-// 	asymbol **loc = NULL;
-// 	loc = malloc(store);
-// 	long location = bfd_canonicalize_symtab(abfd, loc);
-// 	// printf("%lx",location);
-// 	write(1,&location,sizeof(location));
-// 	write(1,nextln,strlen(nextln));
-// }
 
 int main(int argc, char **argv){
 	//char *target = default_target;
@@ -55,36 +23,6 @@ int main(int argc, char **argv){
 		write(1,"FAIL",5);
 		write(1,nextln,strlen(nextln));
 	}
-
-	// bfd_init();
-
-	// bfd *abfd;
-	// abfd = bfd_openr (argv[1],default_target);
-
-	// if(abfd == NULL)
-	// {
-	// 	char *nfound = "File not found!\n";
-	// 	write(1,nfound,strlen(nfound));
-	// 	return 0;
-	// }
-
-	// if (bfd_check_format(abfd, bfd_object))
-	// {
-	// 	char *hds = "Sections:\nName          	VMA      Size          Position\n";
-	// 	write(1,hds,strlen(hds));
-	// 	bfd_map_over_sections(abfd, print_objdump, NULL);
-	// }
-	// else{
-	// 	char *nobject = ": the file is not recognized as a valid object file\n";
-	// 	write(1,argv[1],strlen(argv[1]));
-	// 	write(1,nobject,strlen(nobject));
-	// 	return 0;
-	// }
-
-	// if(bfd_check_format(file, bfd_archive))
-	// {
-	// 	bfd *last = NULL;
-	// }
 	
 	return 0;
 }
---
+++ getsections_dl.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dlfcn.h>
+#include "objsect.h"
+
+static char *nextln = "\n";
+
+#define RDTSC(var)
+{                                                            
+    uint32_t var##_lo, var##_hi;
+    asm volatile ("lfence\n\trdtsc" : "=a"(var##_lo), "=d"(var##_hi));
+    var = var##_hi;
+    var <<= 32;
+    var |= var##_lo;
+}
+
+unsigned long long start, finish;
+
+void run_stats(int flagn){
+	float total = 0;
+	int i = 0;
+	char *fname = "stats";
+	char *flag = if(flagn==0x1)? "RDTL_LAZY" :"RDTL_NOW";
+	int fd = open(fname, O_RDWR|O_CREAT|O_EXCL);
+	write(fd,"getsections_dl.c | ", 20);
+	write(fd,flag, strlen(flag));
+	write(1,nextln, strlen(nextln));
+	do
+	{
+		RDTSC(start);
+		handle = dlopen("libobjdata.so",flagn);
+		RDTSC(finish);
+		//cpu MHz		: 2599.994
+		total += ((finish-start)/2599.994);
+		i++;
+	} while (i<50);
+
+	write(fd,"Avgtime: ",10);
+	char *tempavg = malloc(16*sizeof(char));
+	gcvt(total/50, 5, tempavg);
+	write(fd, tempavg,strlen(tempavg));
+	write(1,nextln, strlen(nextln));
+}
+
+int main(int argc, char **argv){
+
+	if(argc != 3){
+		char *instruct = "Usage: getsections filename flag\n";
+		// printf("Usage: getsections filename\n");
+		write(1,instruct,strlen(instruct));
+		write(1,nextln,strlen(nextln));
+		return 0;
+	}
+
+	const char *flag = argv[2];
+	// //set flagn to 0x1, which is RTLC_LAZY; 0x2 for NOW
+	// int flagn = 1;
+	if(strcmp(flag,"RTLD_LAZY"!= 0)){flagn = 0x2;}
+
+	RDTSC(start);
+	handle = dlopen("libobjdata.so",flagn);
+	RDTSC(finish);
+	//cpu MHz		: 2599.994
+	float time = ((finish-start)/2599.994);
+
+	run_stats(flagn);
+
+	if(enter_objsect(argv[1])){
+		write(1,"PASS",5);
+		write(1,nextln,strlen(nextln));
+	}else{
+		write(1,"FAIL",5);
+		write(1,nextln,strlen(nextln));
+	}
+	
+	return 0;
+}
