c6fa27592a67cdb1c2d5f7cc2f043a4ff3466cf3
Qingyang Shi <Rosesqy@users.noreply.github.com>
2020-09-30 15:32:48 +0800

    add objsect

--- getsections.c
+++ getsections.c
@@ -2,47 +2,46 @@
 #include <string.h>
 #include <fcntl.h>
 // #include <stdio.h>
-#include "bfd.h"
+// #include "bfd.h"
+# include "object.h"
 
-static char *default_target = NULL;
-char *sep = "  ";
-char *nextln = "\n";
+// static char *default_target = NULL;
 
-static void print_objdump(bfd *abfd, asection *sect, void *obj){
+// static char *default_target = "elf64-x86-64";
+// char *sep = "  ";
+// char *nextln = "\n";
+
+// static void print_objdump(bfd *abfd, asection *sect, void *obj){
 	
-	const char *namep;
+// 	const char *namep;
 
-	namep = bfd_section_name(abfd,sect);
-	write(1,namep,strlen(namep));	
-	write(1,sep,strlen(sep));
+// 	namep = bfd_section_name(abfd,sect);
+// 	write(1,namep,strlen(namep));	
+// 	write(1,sep,strlen(sep));
 
-	bfd_vma addr = bfd_section_vma(abfd,sect);
-	// printf("%lx  ",addr);
-	write(1,&addr,sizeof(addr));
-	write(1,sep,strlen(sep));
+// 	bfd_vma addr = bfd_section_vma(abfd,sect);
+// 	// printf("%lx  ",addr);
+// 	write(1,&addr,sizeof(addr));
+// 	write(1,sep,strlen(sep));
 
-	unsigned long size = bfd_section_size(abfd, sect);
-	write(1,&size, sizeof(size));
-	write(1,sep,strlen(sep));
-	
-	long store = bfd_get_symtab_upper_bound(abfd);
-	// printf("%lx  ",size);
-	// write(1,&store,sizeof(store));
-	// write(1,sep,strlen(sep));
-	asymbol **loc = NULL;
-	loc = malloc(store);
-	long location = bfd_canonicalize_symtab(abfd, loc);
-	// printf("%lx",location);
-	write(1,&location,sizeof(location));
-	write(1,nextln,strlen(nextln));
-}
+// 	unsigned long size = bfd_section_size(abfd, sect);
+// 	write(1,&size, sizeof(size));
+// 	write(1,sep,strlen(sep));
+
+// 	long store = bfd_get_symtab_upper_bound(abfd);
+// 	// printf("%lx  ",size);
+// 	// write(1,&store,sizeof(store));
+// 	// write(1,sep,strlen(sep));
+// 	asymbol **loc = NULL;
+// 	loc = malloc(store);
+// 	long location = bfd_canonicalize_symtab(abfd, loc);
+// 	// printf("%lx",location);
+// 	write(1,&location,sizeof(location));
+// 	write(1,nextln,strlen(nextln));
+// }
 
 int main(int argc, char **argv){
 	char *target = default_target;
-	
-	// int fd[2];
-	// fd[0] = open("temp.txt",O_RDWR|O_CREAT|O_EXCL);
-	// fd[1] = open("temp.txt",O_RDWR);
 
 	if(argc != 2){
 		char *instruct = "Usage: getsections filename\n";
@@ -51,13 +50,13 @@ int main(int argc, char **argv){
 		return 0;
 	}
 
-	//unsigned int bfd_init(void);
+	bfd_init();
+
 	bfd *abfd;
 	abfd = bfd_openr (argv[1],default_target);
 
 	if(abfd == NULL)
 	{
-		// printf("File not found!\n");
 		char *nfound = "File not found!\n";
 		write(1,nfound,strlen(nfound));
 		return 0;
@@ -66,14 +65,11 @@ int main(int argc, char **argv){
 	if (bfd_check_format(abfd, bfd_object))
 	{
 		char *hds = "Sections:\nName          	VMA      Size          Position\n";
-		// printf("Sections:\n");
-		// printf("Name          VMA      Size          Position\n");
 		write(1,hds,strlen(hds));
 		bfd_map_over_sections(abfd, print_objdump, NULL);
 	}
 	else{
 		char *nobject = ": the file is not recognized as a valid object file\n";
-		// printf("%c: the file is not recognized as a valid object file\n",argv[1]);
 		write(1,argv[1],strlen(argv[1]));
 		write(1,nobject,strlen(nobject));
 		return 0;
---
+++ objsect.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include "bfd.h"
+
+
+static char *default_target = "elf64-x86-64";
+char *sep = "  ";
+char *nextln = "\n";
+
+
+char *long_to_string(unsigned long val, char *temp){
+
+	int i = 0;
+	char *base = "0123456789abcdef";
+
+	while(val){
+		temp[i++] = base[val%16];
+		val /= 16;
+	}
+
+	// char *stringnow = '0000000000000000';
+	// for(int j = 0; j<16;j++){
+	// 	stringnow[j] = temp[15-]
+	// }
+	
+	return temp;
+}
+
+void print_objdump(bfd *abfd, asection *sect, void *obj){
+
+	const char *namep;
+	// //create a space for later temporary string
+	// char *temp = malloc(16*sizeof(char));
+	//initialize a temp charset for later use
+	char temp[16] = '0000000000000000';
+
+	namep = bfd_section_name(abfd,sect);
+	write(1,namep,strlen(namep));	
+	write(1,sep,strlen(sep));
+
+	// bfd_vma vma = bfd_section_vma(abfd,sect);
+	// printf("%lx  ",vma);
+	char *vma = long_to_string(bfd_section_vma(abfd,sect),temp);
+	// write(1,&vma,sizeof(vma));
+	write(1,vma,strlen(vma));
+	write(1,sep,strlen(sep));
+
+	unsigned long size = bfd_section_size(abfd, sect);
+	// write(1,&size, sizeof(size));
+	char *size = long_to_string(size,temp);
+	write(1,size,strlen(size));
+	write(1,sep,strlen(sep));
+
+	long store = bfd_get_symtab_upper_bound(abfd);
+	asymbol **loc = NULL;
+	loc = malloc(store);
+	long location = bfd_canonicalize_symtab(abfd, loc);
+	// printf("%lx",location);
+	// write(1,&location,sizeof(location));
+	char *location = long_to_string(location,temp);
+	write(1,location,strlen(location));
+	write(1,nextln,strlen(nextln));
+}
+
